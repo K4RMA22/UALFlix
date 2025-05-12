6af70bf9447af2b9069c6eb7ee0f7fa04b0329a1
IK4RM4 <joao.p.22@hotmail.com>
2025-05-12 22:17:33 +0100

    Revert "Teste commit"
    This reverts commit 5073cc160cb440d46deead370db80316dc5fee35.

--- catalog_service/app.py
+++ catalog_service/app.py
@@ -10,11 +10,11 @@ os.makedirs(VIDEO_FOLDER, exist_ok=True)
 
 @app.route('/upload', methods=['POST'])
 def upload_video():
-    title = request.form.get('title','')
-    description = request.form.get('description','')
+    title = request.form['title']
+    description = request.form['description']
     file = request.files['file']
 
-    if file and file.filename:
+    if file:
         timestamp = str(int(time.time()))
         safe_filename = timestamp + "_" + file.filename
         filepath = os.path.join(VIDEO_FOLDER, safe_filename)
@@ -33,9 +33,7 @@ def upload_video():
         cur.close()
         conn.close()
 
-        return jsonify({"message": "Video uploaded successfully!",
-                        "filename": safe_filename,
-                        "url": url}), 200
+        return jsonify({"message": "Video uploaded successfully!"}), 200
     else:
         return jsonify({"error": "No file uploaded"}), 400
 
--- streaming_service/app.py
+++ streaming_service/app.py
@@ -3,12 +3,12 @@ import os
 
 app = Flask(__name__)
 
-VIDEO_FOLDER = '/videos'
-os.makedirs(VIDEO_FOLDER, exist_ok=True)
+UPLOAD_FOLDER = '/app/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/stream/<filename>')
 def stream_video(filename):
-    return send_from_directory(VIDEO_FOLDER, filename)
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=True)
